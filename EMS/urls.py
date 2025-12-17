6ac600f4b11314ffda72fd3a49c50169e9f8d701
sharmaRoushan-Official <roushankr26@gmail.com>
2025-12-17 17:40:32 +0530

    Add and Update URL

--- EMS/urls.py
+++ EMS/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     # path('dtlFor/',viewHome2), # http://127.0.0.1:8000/EMS/dtlFor/
     # path('dummyEmp/',view_home), # http://127.0.0.1:8000/EMS/dummpyEmp/
     path('insertems/',view_insert_ems),
+    path('customer/',viewCustomer, name='customer'),
     
 ]
