09f67b2dbe24cbb44e9258a7aa0c2af540d0d857
sharmaRoushan-Official <roushankr26@gmail.com>
2025-12-31 16:14:10 +0530

    Remove unnecessary blank line in urls.py

--- SMS/urls.py
+++ SMS/urls.py
@@ -5,5 +5,4 @@ from SMS.views import *
 urlpatterns = [
     path('stuReg/',view_student_registration), # http://127.0.0.1:8000/SMS/stuReg/
     path('createPay/',viewPayment,name="viewPayment"),
-    
 ]
