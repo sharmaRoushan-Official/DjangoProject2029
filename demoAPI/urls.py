04abd845ab4c68ca02545a101fe3482277aa18e1
sharmaRoushan-Official <roushankr26@gmail.com>
2026-02-03 17:31:59 +0530

    Add CRUD functionality for Trainer model; implement create, update, and partial update views

--- demoAPI/urls.py
+++ demoAPI/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('firstApi/', view_first_api, name='first-api'), # http://127.0.0.1:8000/demoapi/firstAPI/
     path('secondApi/', view_second_api, name='second-api'), # http://127.0.0.1:8000/demoapi/secondApi/
     path('trainers/', trainer_list, name='trainer-list'), # http://127.0.0.1:8000/demoapi/trainers/
+    path('createTrainer/', create_trainer, name='create-trainer'), # http://127.0.0.1:8000/demoapi/createTrainer/
+    path('updateTrainer/<int:id>/', update_trainer, name='update-trainer'), # http://127.0.0.1:8000/demoapi/updateTrainer/1/
+    path('partialUpdateTrainer/<int:id>/', partial_update_trainer, name='partial-update-trainer'), # http://127.0.0.1:8000/demoapi/partialUpdateTrainer/1/
 ]
--- demoAPI/views.py
+++ demoAPI/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from demoAPI.models import Trainer
 from demoAPI.serializers import TrainerSerializer
+from rest_framework import status
 
 # Create your views here.
 
@@ -26,5 +27,58 @@ def trainer_list(request):
         # print("Trainders:",trainers)
         serializer = TrainerSerializer(trainers,many=True)
         return Response(serializer.data)
+# GET - Fetching the data
+# many=True - when we are fetching multiple records
+# many=False - when we are fetching single record
+# return Response (serializer.data) - it returns the serialized data as JSON response
+
+# Post - Creating new record
+
+@api_view(http_method_names=['POST'])
+def create_trainer(request):
+    serializer = TrainerSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)  # HTTP 201 Created
+    return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)  # HTTP 400 Bad Request
+
+# request.data =- JSON input data from the client
+# is_valid() - validates the input data against the model fields
+# save() - saves the new record to the database
+
+
+# PUT - Update Full object 
+
+@api_view(http_method_names=['PUT'])
+def update_trainer(request, id):
+    trainer = Trainer.objects.get(id=id)
+    serializer = TrainerSerializer(trainer,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST) 
+
+# PUT - Updates all fields of the existing record
+# missing field - error 
+# Used for complete replacement
+
+
+# PATCH - Update Partial object
+
+
+@api_view(http_method_names=['PATCH'])
+def partial_update_trainer(request, id):
+    trainer = Trainer.objects.get(id=id)
+    serializer = TrainerSerializer(trainer,data=request.data,partial=True) # key difference is here
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+# PATCH - Updates only specified fields of the existing record
+# partial = True -> allows missing fields 
+# Most used in real projects 
+
+
     
 
